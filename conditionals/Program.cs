a868c02c2a790e5ef82db453efe6a9f24df6c2d2
Joseph <josephmacdonald4629@gmail.com>
2023-09-25 09:34:43 -0300

    add conditionals

--- conditionals/Program.cs
+++ conditionals/Program.cs
@@ -8,8 +8,59 @@ namespace conditionals
 {
     internal class Program
     {
+        static int health;
+        static int lives;
         static void Main(string[] args)
         {
+            Console.WriteLine("Conditionals");
+            Console.WriteLine("------------");
+            Console.WriteLine("");
+
+            // initializations
+            health = 100;
+            lives = 1;
+
+            // sample gameplay
+
+            ShowHud();
+            TakeDamage(10);
+            Console.WriteLine("");
+            ShowHud();
+            TakeDamage(40);
+            Console.WriteLine("");
+            ShowHud();
+            TakeDamage(60);
+            Console.WriteLine("");
+            ShowHud();
+
+
+            Console.WriteLine("");
+            Console.WriteLine("Press any key to exit the program");
+            Console.ReadKey(true);
+        }
+
+        static void ShowHud()
+        {
+            Console.WriteLine("Health: " + health);
+            Console.WriteLine("Lives: " + lives);
+        }
+
+        static void TakeDamage(int damage)
+        {
+            health = health - damage;
+
+            if (health <= 0)
+            {
+                Console.WriteLine("GAMEOVER");
+                lives = lives - 1;
+
+                if (lives <= 0)
+                {
+                    Console.WriteLine("Player has DIED");
+                }
+
+                health = 0;
+            }
         }
     }
 }
