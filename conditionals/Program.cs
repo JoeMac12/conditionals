6acaec6457c6f4ab62bd75036f22cc9aab12fbf6
Joseph <144450534+JoeMac12@users.noreply.github.com>
2023-09-26 16:17:46 -0300

    Add Conditionals

--- conditionals/Program.cs
+++ conditionals/Program.cs
@@ -1,66 +1,115 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System;
 
-namespace conditionals
+namespace FinalHud
 {
     internal class Program
     {
-        static int health;
-        static int lives;
+        private static int Health = 100;
+        private static int Shield = 50;
+        private static int Lives = 1;
+        private static float ScoreMultiplier = 0f;
+        private static int Weapon = 0;
+        private static string[] WeaponNames = { "Pistol", "Shotgun", "Spreader", "Laser", "Sniper", "BFG" };
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Conditionals");
+            Console.WriteLine("WELCOME TO CAVE ESCAPE");
+            Console.WriteLine("----------------------");
+
+            ShowHUD();
+
+            Console.WriteLine("------------");
+            Console.WriteLine("START GAME");
+            Console.WriteLine("");
+
+            Console.WriteLine("You Awaken in a dark cave, cold and damp. Torch in hand, you begin your perilous journey back to the surface.");
+
+            Console.WriteLine("As you enter a new part of the cave, you come across a goblin with an old rusty sword");
+            Console.WriteLine("");
+            Console.WriteLine("It Attacks You!");
+
+            TakeDamage(30);
+
             Console.WriteLine("------------");
             Console.WriteLine("");
-
-            // initializations
-            health = 100;
-            lives = 1;
-
-            // sample gameplay
-
-            ShowHud();
-            TakeDamage(10);
+            Console.WriteLine("You do a counter-attack and kill the goblin!");
             Console.WriteLine("");
-            ShowHud();
-            TakeDamage(40);
-            Console.WriteLine("");
-            ShowHud();
-            TakeDamage(60);
-            Console.WriteLine("");
-            ShowHud();
+            Console.WriteLine("------------");
 
+            AddScore(15, 0.5f);
 
+            Console.WriteLine("------------");
             Console.WriteLine("");
-            Console.WriteLine("Press any key to exit the program");
+            Console.WriteLine("You come across an underwater waterfall inside the cave. You see a Giant bat emerge from behind the waterfall!");
+            Console.WriteLine("It spits mutated acid at you!");
+            Console.WriteLine("");
+            Console.WriteLine("------------");
+
+            TakeDamage(50);
+
+            Console.WriteLine("------------");
+            Console.WriteLine("");
+            Console.WriteLine("The bat flies away before you can attack it!");
+            Console.WriteLine("");
+            Console.WriteLine("After a long journey to the surface, you begin to see the light from the sun on the cold cave rocks.");
+            Console.WriteLine("Just before you reach the exit, a huge troll blocks your path. It roars and swings its massive club right on top of you, ending your journey!");
+            Console.WriteLine("");
+            Console.WriteLine("------------");
+
+            TakeDamage(100);
+
+            Console.WriteLine("------------");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("GAME OVER");
+            Console.ResetColor();
+
             Console.ReadKey(true);
         }
 
-        static void ShowHud()
+        private static void ShowHUD()
         {
-            Console.WriteLine("Health: " + health);
-            Console.WriteLine("Lives: " + lives);
+            Console.WriteLine("Player Stats");
+            Console.WriteLine("------------");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("HP: " + Health);
+            Console.WriteLine("Shield: " + Shield);
+            Console.WriteLine("Lives: " + Lives);
+            Console.WriteLine("Weapon: " + WeaponNames[Weapon]);
+            Console.WriteLine("Score Multiplier: " + ScoreMultiplier);
+            Console.ResetColor();
+            Console.WriteLine("Health Status: " + GetHealthStatus());
+        }
+        private static void TakeDamage(int damage)
+        {
+            Health -= damage;
+            if (Health <= 0)
+            {
+                Health = 0;
+                Lives--; 
+                ScoreMultiplier = 0; 
+            }
+
+            ShowHUD();
         }
 
-        static void TakeDamage(int damage)
+        private static string GetHealthStatus()
         {
-            health = health - damage;
+            if (Health <= 10)
+                return "Imminent Danger";
+            else if (Health <= 50)
+                return "Badly Hurt";
+            else if (Health <= 75)
+                return "Hurt";
+            else if (Health < 100)
+                return "Healthy";
+            else
+                return "Perfect Health";
+        }
 
-            if (health <= 0)
-            {
-                Console.WriteLine("GAMEOVER");
-                lives = lives - 1;
-
-                if (lives <= 0)
-                {
-                    Console.WriteLine("Player has DIED");
-                }
-
-                health = 0;
-            }
+        private static void AddScore(int pointsearned, float scoremultiplier)
+        {
+            ScoreMultiplier += pointsearned * scoremultiplier;
+            ShowHUD();
         }
     }
 }
